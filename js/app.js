6420c35c6ccec86c3dafb4c1dc197cfecb5945a6
Roman Dyukoff <hlebahleb@gmail.com>
2022-05-20 17:42:42 +0300

    fix bug

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 const burgerBtn = document.querySelector('.header__burger');
+const headerMenu = document.querySelector('.header__menu');
+const body = document.querySelector('body');
 
 burgerBtn.addEventListener('click', dropBurger);
 
 function dropBurger() {
-    const headerMenu = document.querySelector('.header__menu');
-    const body = document.querySelector('body');
 
     headerMenu.classList.toggle('active');
     burgerBtn.classList.toggle('active');
